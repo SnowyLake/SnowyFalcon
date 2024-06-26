229f019148926f8d6c1c4963c01fbe2e74aaf79d
SnowyLake <snowylake.dev@outlook.com>
2024-06-27 01:11:15 +0800

    [Feature]自定义配置读取与Log类型, ShaderRemapping函数封装

--- Plugins/SnowyFalcon/Source/SnowyFalcon/Public/SnowyFalcon.h
+++ Plugins/SnowyFalcon/Source/SnowyFalcon/Public/SnowyFalcon.h
@@ -7,8 +7,4 @@ class FSnowyFalconModule final : public IModuleInterface
 public:
 	virtual void StartupModule() override;
 	virtual void ShutdownModule() override;
-	
-private:
-	static const inline FString EngineDefaultShaderVirtualDir = TEXT("/Engine");
-	static const inline FString EngineDefaultShaderRealDir = FPaths::Combine(*(FPaths::EngineDir()), TEXT("Shaders"));
 };
---
+++ Plugins/SnowyFalcon/Source/SnowyFalcon/Public/Utils/SnowyFalconSetting.h
@@ -0,0 +1,36 @@
+#pragma once
+
+#include "CoreMinimal.h"
+
+SNOWYFALCON_API DECLARE_LOG_CATEGORY_EXTERN(LogSnowyFalcon, Verbose, All);
+
+namespace SnowyFalcon
+{
+class SNOWYFALCON_API FSetting
+{
+public:
+	static FSetting& Get();
+
+public:
+	// ----------- Configs -----------
+	// Shader Remapping
+	bool bShaderSourceRemapping;
+	FString ShaderSourceRemappingDirectory;
+
+private:
+	FSetting();
+	~FSetting() = default;
+	FSetting(const FSetting&) = delete;
+	FSetting& operator=(const FSetting&) = delete;
+	FSetting(FSetting&&) = delete;
+	FSetting& operator=(FSetting&&) = delete;
+
+private:
+	void LoadConfig(const FString& ConfigPath);
+
+	static void GetValueBool(const FConfigFile& ConfigFile, const TCHAR* Section, const TCHAR* Key, bool& Value);
+	static void GetValueString(const FConfigFile& ConfigFile, const TCHAR* Section, const TCHAR* Key, FString& Value);
+	static void GetValueArray(const FConfigFile& ConfigFile, const TCHAR* Section, const TCHAR* Key, TArray<FString>& Value);
+};
+}
+
